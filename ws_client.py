e98ea86532af7284ae009a2cd5ffffd05e53db0d
kaushik <kaushik.codes.112@gmail.com>
2026-06-07 12:46:08 +0530

    changed readlines to readline

---
+++ ws_client.py
@@ -0,0 +1,32 @@
+import websockets,asyncio,sys
+
+class Ws_client:
+    def __init__(self) -> None:
+        pass
+    async def send_msg(self,websocket):
+        loop = asyncio.get_event_loop()
+        while True:
+            message = await loop.run_in_executor(None,sys.stdin.readline)
+            message = message.strip()
+            if message:
+                await websocket.send(message)
+
+    async def recv_msg(self,websocket):
+        try:
+            async for message in websocket:
+                print(f"\n[Incoming]: {message}")
+        except websockets.exceptions.ConnectionClosed:
+            print("disconnected from server")
+    async def main(self):
+        #Here i need to connect to the server 
+        #using websockets
+        uri = "ws://localhost:1234" # 1234 is the port number where server is listeninging
+        print(f"Connecting to uri: {uri}")
+        async with websockets.connect(uri) as websocket:
+            print("Connected successfully!")
+            await asyncio.gather(self.send_msg(websocket),self.recv_msg(websocket))
+        pass
+
+if __name__ =="__main__":
+    client = Ws_client()
+    asyncio.run(client.main())
